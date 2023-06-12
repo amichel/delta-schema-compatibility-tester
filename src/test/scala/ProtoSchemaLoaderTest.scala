b7e319da56f9e571f0de9b7e9ec76809c1e6efda
amishel <amishel@gmail.com>
2023-06-12 19:07:45 -0400

    Basic tests using proto descriptors

--- src/test/scala/ProtoSchemaLoaderTest.scala
+++ src/test/scala/ProtoSchemaLoaderTest.scala
@@ -2,11 +2,16 @@ package org.amishel.utils
 
 import org.scalatest.funsuite.AnyFunSuiteLike
 
+import java.nio.file.{FileSystems, Paths}
+
 class ProtoSchemaLoaderTest extends AnyFunSuiteLike {
 
   test("testLoadAsSparkSchema") {
+    val fileSeparator = FileSystems.getDefault.getSeparator
+    val descriptorBasePath = Paths.get(System.getProperty("user.dir"), "target", "scala-2.13", "resource_managed", "test", "protoc_descriptors") + fileSeparator
+
     //TODO: for some reason loading resources via getClass.getResource() or getClassLoader does not find the descriptor
-    val descriptorPath = s"${System.getProperty("user.dir")}\\target\\scala-2.13\\resource_managed\\test\\protoc_descriptors\\address.descr"
+    val descriptorPath = descriptorBasePath + "address.descr"
     val target = new ProtoSchemaLoader(descriptorPath, "Address")
     val actual = target.loadAsSparkSchema()
     assert {
--- src/test/scala/ProtoSchemaValidatorTest.scala
+++ src/test/scala/ProtoSchemaValidatorTest.scala
@@ -2,13 +2,16 @@ package org.amishel.utils
 
 import org.scalatest.funsuite.AnyFunSuiteLike
 
+import java.nio.file.{FileSystems, Paths}
+
 class ProtoSchemaValidatorTest extends AnyFunSuiteLike {
 
   test("testProtoValidator") {
-    //TODO: for some reason loading resources via getClass.getResource() or getClassLoader does not find the descriptor
-    val descriptorBasePath = s"${System.getProperty("user.dir")}\\target\\scala-2.13\\resource_managed\\test\\protoc_descriptors\\"
+    val fileSeparator = FileSystems.getDefault.getSeparator
+    val descriptorBasePath = Paths.get(System.getProperty("user.dir"), "target", "scala-2.13", "resource_managed", "test", "protoc_descriptors") + fileSeparator
 
     val testDefinition = Array(
+      //TODO: for some reason loading resources via getClass.getResource() or getClassLoader does not find the descriptor
       ("person_v1.descr", "person_v2_compatible_with_v1.descr", true),
       ("person_v2_compatible_with_v1.descr", "person_v2.1_incompatible_with_v2.descr", false),
       ("person_v2_compatible_with_v1.descr", "person_v2.2_incompatible_with_v2.descr", false),
